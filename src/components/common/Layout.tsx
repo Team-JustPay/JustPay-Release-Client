e2fc3bca3619e3c40a2cd7691b14f00a3c418163
YOOJS1205 <junsang8921@gmail.com>
2023-05-21 18:02:05 +0900

    기능이 있는 헤더 제작

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,7 +1,8 @@
 import React, { ReactElement } from 'react';
 import styled from 'styled-components';
 
-import MainHeader from './MainHeader';
+import FuncHeader from './header/FuncHeader';
+import MainHeader from './header/MainHeader';
 
 interface LayoutProps {
   children?: ReactElement[];
@@ -12,6 +13,10 @@ export default function Layout({ children }: LayoutProps) {
     <Container>
       {/* 헤더: 확인된 헤더 종류는 2가지, 라우터 체크해서 동적으로 넣어주기 */}
       <MainHeader />
+      <FuncHeader title="거래 상세 내역" leftFunc rightFunc="close" />
+      <FuncHeader title="거래글" leftFunc rightFunc="more" />
+      <FuncHeader title="내 정보" leftFunc rightFunc="modify" />
+      {/* 메인 컨텐츠 */}
       {children}
     </Container>
   );
