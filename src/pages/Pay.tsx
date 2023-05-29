2ac56d9d3751edd3084b7c44e6da0af8035c77dd
wooyoung0830 <thddndud0830@gmail.com>
2023-05-29 17:12:11 +0900

    feat : 게시글 정보 컨테이너

--- src/pages/Pay.tsx
+++ src/pages/Pay.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import Layout from 'components/common/Layout';
-import PayWarning from 'components/pay/PayWarning';
-import AddressInformation from 'components/common/AddressInformation';
 import styled from 'styled-components';
+import Layout from 'components/common/Layout';
+import PostInformation from 'components/PostInformation';
+import AddressInformation from 'components/common/AddressInformation';
+import PayWarning from 'components/pay/PayWarning';
 
 export default function Pay() {
+  const dummyPost = {
+    postThumbnail:
+      'https://github.com/Team-JustPay/JustPay-Release-Client/assets/62867581/6735d47e-d79d-425d-9ed0-96a58ece02bb',
+    postTitle: 'NCTDREAM Best Friend Ever 포카 양도합니다 ',
+    postWriter: {
+      userThumbnail:
+        'https://github.com/Team-JustPay/JustPay-Release-Client/assets/62867581/0debb408-1969-4372-9863-5f0ae7082f4f',
+      userName: '거래계',
+      userTwitterId: '@music_stars',
+    },
+    // postDate: '23.08.03', // 4~ 뷰에서 사용
+  };
   return (
     <Layout>
+      <PostInformation
+        postThumbnail={dummyPost.postThumbnail}
+        postTitle={dummyPost.postTitle}
+        postWriter={dummyPost.postWriter}
+      />
       <Title>결제 예정 금액</Title>
       <BorderLine />
       <Title>받는 사람</Title>
