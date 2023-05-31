0b7a2a06b2edd40bdabee25755b82cd21d3c0b05
wooyoung0830 <thddndud0830@gmail.com>
2023-05-31 09:56:35 +0900

    feat : react suspense 추가

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes as PageRoutes, Route as PageRoute } from 'react-router-dom';
 
 const HomePage = lazy(() => import('pages/Home'));
@@ -8,10 +8,12 @@ const PayPage = lazy(() => import('pages/Pay'));
 export default function Routes() {
   return (
     <BrowserRouter>
-      <PageRoutes>
-        <PageRoute path="/" element={<HomePage />} />
-        <PageRoute path="/pay" element={<PayPage />} />
-      </PageRoutes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <PageRoutes>
+          <PageRoute path="/" element={<HomePage />} />
+          <PageRoute path="/pay" element={<PayPage />} />
+        </PageRoutes>
+      </Suspense>
     </BrowserRouter>
   );
 }
